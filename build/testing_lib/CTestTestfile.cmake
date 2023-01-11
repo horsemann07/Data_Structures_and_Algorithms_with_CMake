c909f4ea8765b6d87eb86fe20e6c2e2d98ea6a1b
horsemann07 <raghavjha1531@gmail.com>
2023-01-12 00:18:29 +0530

    updated: linkedlists new algorithm  updated and some fixed

--- build/testing_lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/horsemann/Desktop/WorkSpace/Personal/data_structure_in_c/testing_lib
-# Build directory: /home/horsemann/Desktop/WorkSpace/Personal/data_structure_in_c/build/testing_lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("unity")
--- build/testing_lib/unity/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/horsemann/Desktop/WorkSpace/Personal/data_structure_in_c/testing_lib/unity
-# Build directory: /home/horsemann/Desktop/WorkSpace/Personal/data_structure_in_c/build/testing_lib/unity
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
